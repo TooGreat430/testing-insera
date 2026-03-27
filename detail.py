d3c8eee3718cce01f5915f4aea3347fa5f87bf35
unknown <mikhael.celieano@metrodata.co.id>
2026-03-27 14:06:46 +0700

    Schema Enum

--- detail.py
+++ detail.py
@@ -169,7 +169,7 @@ DETAIL_LINE_SCHEMA_TEXT = """{
   "pl_item_no": "string",
   "pl_description": "string",
   "pl_quantity": "number",
-  "pl_package_unit": {"type": "string", "enum": ALLOWED_PACKAGE_UNIT},
+  "pl_package_unit": {"type": "string", "enum": """,ALLOWED_PACKAGE_UNIT,"""},
   "pl_package_count": "number",
   "pl_nw": "number",
   "pl_gw": "number",
@@ -558,7 +558,7 @@ GENERAL KNOWLEDGE DETAIL:
    - PAHAMI TERLEBIH DAHULU JENIS PACKAGE UNIT YANG DIGUNAKAN PADA DOKUMEN.
    - Tentukan package unit berdasarkan struktur kemasan yang ada.
    - Ada beberapa penempatan Package Unit:
-      - Di Header dari package Unit contoh: Header mengatakan "PCS/CTN" Berarti unit yang digunakan yaitu "CTN"
+      - Di Header dari package Unit contoh: Header mengatakan "PCS/CTN" Berarti unit yang digunakan yaitu "CT"
       - Terdapat kolom tambahan untuk package unit contoh terdapat 2 kolom Package count dan juga unitnya. Berarti value unit akan mengikuti kolom dari unitnya.
       - Terdapat bersebelahan langsung dengan value dari package count. Contoh:: 25 SET berarti unit yang digunakan "SET". 25 Carton berarti unit yang digunakan "CTN".
    - TOLONG PAHAMI penempatan dari package count.
